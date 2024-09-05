2a408ab96ff109de733ced21c54a2e362d1c4157
alex2000d <demarcoalessandro777@gmail.com>
2024-09-05 15:52:09 +0200

    [UPD] arrey display with var_dump

--- index.php
+++ index.php
@@ -38,6 +38,8 @@
     ],
 
 ];
+
+var_dump( $hotels)
 ?>
 
 <!DOCTYPE html>
@@ -45,7 +47,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>php hotel</title>
 </head>
 <body>
     
